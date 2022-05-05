83891d82b9501787ec4f07e0d2cfaefeb4665374
Mike <mihaildaryin@gmail.com>
2022-05-05 21:29:11 +0300

    добавлена маршрутизация для ссылок sidebar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Profile from "./components/Profile/Profile";
+import Messages from "./components/Messages/Messages";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <Sidebar />
-      <Profile />
-      {/* <Messages />
-      <News />
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/messages" element={<Messages />} />
+        {/* <News />
       <Music />
       <Settings />
       <Friends /> */}
+      </Routes>
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,42 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import style from "./Sidebar.module.css";
 const Sidebar = () => {
+  const activeLink = () => {
+    return (sideData) => (sideData.activeLink ? style.active : style.item);
+  };
   return (
     <div className={style.sidebar}>
-      <div>Profile</div>
-      <div>Messages</div>
-      <div>News</div>
-      <div>Music</div>
-      <div>Settings</div>
-      <div>Friends</div>
+      <div>
+        <NavLink to="/profile" className={activeLink()}>
+          Profile
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/messages" className={activeLink()}>
+          Messages
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/news" className={activeLink()}>
+          News
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/music" className={activeLink()}>
+          Music
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/settings" className={activeLink()}>
+          Settings
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/friends" className={activeLink()}>
+          Friends
+        </NavLink>
+      </div>
     </div>
   );
 };
