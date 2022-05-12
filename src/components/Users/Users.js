3e82649cb6d0e1b8b9584421e404598467e2dec7
Mike <mihaildaryin@gmail.com>
2022-05-12 22:45:31 +0300

    компонент Users переписан с функционального на классовый

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,45 +1,51 @@
 import React from "react";
 import axios from "axios";
 // import User from "./User/User";
-const Users = (props) => {
-  if (props.usersData.length === 0) {
-    axios.get("http://localhost:3001/users").then((response) => {
-      props.setUsers(response.data);
-    });
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    if (this.props.usersData.length === 0) {
+      axios.get("http://localhost:3001/users").then((response) => {
+        this.props.setUsers(response.data);
+      });
+    }
   }
 
-  return (
-    <div>
-      {props.usersData.map((user) => (
-        <div key={user.id}>
-          <div>{user.avatar}</div>
-          <div>{user.name}</div>
-          <div>{user.status}</div>
-          <div>
-            {user.follow ? (
-              <button
-                onClick={() => {
-                  props.follow(user.id);
-                }}
-              >
-                {" "}
-                UNFOLLOW{" "}
-              </button>
-            ) : (
-              <button
-                onClick={() => {
-                  props.unfollow(user.id);
-                }}
-              >
-                {" "}
-                FOLLOW{" "}
-              </button>
-            )}
+  render() {
+    return (
+      <div>
+        {this.props.usersData.map((user) => (
+          <div key={user.id}>
+            <div>{user.avatar}</div>
+            <div>{user.name}</div>
+            <div>{user.status}</div>
+            <div>
+              {user.follow ? (
+                <button
+                  onClick={() => {
+                    this.props.follow(user.id);
+                  }}
+                >
+                  {" "}
+                  UNFOLLOW{" "}
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    this.props.unfollow(user.id);
+                  }}
+                >
+                  {" "}
+                  FOLLOW{" "}
+                </button>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
